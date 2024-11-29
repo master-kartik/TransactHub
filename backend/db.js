4070b5ca7022a589cbf9f7157975ca73d21068fd
Kartik Kochhar <kochharkartik51@gmail.com>
2024-11-29 14:18:42 +0530

    added account schema

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost:27017/paytm")
+
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -27,8 +29,25 @@ const userSchema = new mongoose.Schema({
         maxLength: 50
     }
 });
-  const User = mongoose.model("Users",userSchema);
+const accountSchema = new mongoose.Schema({
+    userId : {
+        type: mongoose.Schema.Types.ObjectId,
+        ref : 'User',
+        required: true
+
+
+    },
+    balance : {
+        type: Number,
+        required: true
+    }
+})
+
+const User = mongoose.model("User",userSchema);
+const Account = mongoose.model("Account",accountSchema)
+
 
 module.exports={
-    User
+    User,
+    Account
 }
